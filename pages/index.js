1bf24a5f71d3883d04fe80dd47572e885389c3c1
sujalsahu <sujalsahu0804@gmail.com>
2023-07-06 11:12:37 +0530

    small changes

--- pages/index.js
+++ pages/index.js
@@ -65,10 +65,8 @@ export default function Home({user,logout,addcart,removecart,clearcart,cart,subt
               <div className={`${styles.slide_in}`}>
                  <h1 className='text-black-700 text-2xl md:text-4xl lg:text-5xl mb-4 md:mb-12 font-bold'>ShoesPedia</h1>
               </div>
-              <div className={`text-black-700 lg:mr-20 ${styles.slide_in}`}>
-                <p className='sm:text-left'>
+              <div className={`text-black-700 lg:mr-20 ${styles.suj} ${styles.slide_in}`}>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi quas veritatis eaque, quis vel quidem. Suscipit totam aliquid, optio beatae alias doloribus perspiciatis. Reprehenderit laboriosam esse, odit cupiditate id accusamus cumque dicta.
-                </p>
               </div>
               <div className={styles.slide_in}>
               <button className='flex items-center space-x-1.5 rounded-lg bg-blue-700 px-4 py-2 text-white duration-100 hover:bg-blue-800 mt-12' onClick={() => scrollToCollections()}>View Collections</button>
